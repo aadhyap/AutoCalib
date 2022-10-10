ef81365c1b54acfc5bd0f1f566c385dbc8f7724e
aadhyaputtur <putturaadhya@gmail.com>
2022-10-10 18:44:17 -0400

    Predicting rotational and translation matrix

--- calib.py
+++ calib.py
@@ -46,11 +46,14 @@ def correspondance():
 
 	allimgs = glob.glob('./Calibration_Imgs/*.jpg')#testing
 
-
+	count = 0
+	V = []
 	for filename in allimgs:
 		img = cv2.imread(filename) 
 		gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 		ret, corners = cv2.findChessboardCorners(gray,(grida, gridb), cv2.CALIB_CB_ADAPTIVE_THRESH + cv2.CALIB_CB_FAST_CHECK + cv2.CALIB_CB_NORMALIZE_IMAGE)
+
+		all_h = []
 
 		#ref: https://learnopencv.com/camera-calibration-using-opencv/
 		if ret == True:
@@ -61,57 +64,169 @@ def correspondance():
 			# display corners
 			img = cv2.drawChessboardCorners(img, (grida, gridb), fincorners, ret)
 
-			#cv2.imshow('img',img)
-			#cv2.waitKey(1000)
 			cv2.imwrite("corners.png", img)
 		
+			X = worldX
 
+			Y = worldY
 
-	
-		print(imgPts)
-		#u = imgPts[:,0]
-		#v = imgPts[:,1]
-		X = worldX
-		Y = worldY
-		matrix = []
-	
-
-		print("all pts")
-		print(imgPts)
-		#imgPts = imgPts.reshape(54,-1)
-
-		curImg = imgPts[0]
+			curImg = imgPts[count] #every image
+			print("Corners ")
+			print(corners)
 		
+			#choose4pts 
+			chosenWorld = [[worldX[0][0], worldY[0][0]], [worldX[1][0], worldY[1][0]], [worldX[0][1], worldY[0][1]], [worldX[1][1], worldY[1][1]]]
+			chosenImg = [[curImg[0][0], curImg[0][1]], [curImg[1][0], curImg[1][1]], [curImg[6][0], curImg[6][1]], [curImg[7][0], curImg[7][1]]]
+			cv2.circle(img, (int(curImg[0][0]),int(curImg[0][1])), 20, (255, 0, 0), 2) #red
+			cv2.circle(img, (int(curImg[1][0]), int(curImg[1][1])), 20, (0, 0, 255), 2) #blue
+			cv2.circle(img, (int(curImg[6][0]), int(curImg[6][1])), 20, (0, 255, 0), 2) #green
+			cv2.circle(img, (int(curImg[7][0]), int(curImg[7][1])), 20, (0, 0, 0), 2) #black
 
-		print("curimg whole ", len(curImg))
-		print("curimg whole ", curImg)
-		#choose4pts 
-		chosenWorld = [[worldX[0], worldY[0]], [worldX[1], worldY[1]], [worldX[6], worldY[6]], [worldX[7], worldY[7]]]
-		chosenImg = [[curImg[0][0], curImg[0][1]], [curImg[1][0], curImg[1][1]], [curImg[6][0], curImg[6][1]], [curImg[7][0], curImg[7][1]]]
-		for coor in chosenImg:
-			x = int(coor[0])
-			y = int(coor[1])
-			cv2.circle(img, (x, y), 4, (0, 0, 255), -1)
-		cv2.imshow('img',img)
-		cv2.imwrite("4corners.png", img)
-		cv2.waitKey(1000)
+			cv2.imwrite("img" + str(count) +".png", img)
+
+			chosenWorld =np.float32(np.array(chosenWorld))
+			chosenImg = np.float32(np.array(chosenImg))
+
+			h = cv2.getPerspectiveTransform(chosenWorld, chosenImg)
+			all_h.append(h)
+
+			v12 = getvij(h, 0,1)
+			v11 = getvij(h, 0,0)
+			v22 = getvij(h, 1,1)
+
+			V.append([v12, np.subtract(v11, v22)])
+
 
 		
-		for i in range(len(chosenImg)):
+			count += 1
+
+
+
+	V = np.array(V)
+	V = V.reshape((-1,V.shape[2]))
+
+
+	u, s, v = np.linalg.svd(V, full_matrices = True) 
+	b = v[-1]
+
+
+	B = getBigB(b)
+
+
 	
-			r1 = [[X[i], Y[i], Z[i], 1], [0, 0, 0, 0], [-(u[i])*X[i], -(u[i])*Y[i], -(u[i])*Z[i], -(u[i])] ]
-			r2 = [[0, 0, 0, 0], [X[i], Y[i], Z[i], 1], [0, 0, 0, 0], [-(v[i])*X[i], -(v[i])*Y[i], -(v[i])*Z[i], -(v[i])]]
-			matrix = [matrix, r1, r2]
-		U, s, VT = svd(matrix)  #ref https://machinelearningmastery.com/singular-value-decomposition-for-machine-learning/
-		Sigma = diag(s)
-		B = U.dot(Sigma.dot(VT))
+	A, lam = intparameters(b)
 
-		print("MATRIX R and T")
-		print(B)
+	for i in range(len(all_h)):
+		R = getRT(A, all_h[i], lam)
+		print(R)
 
+	#centerDx, centerDy = minimize(A[0][0], A[1][1], A[0][2], A[1][2]) #alpha, beta, u0, v0
+
+
+	k = [0,0]
+
+	#b = littleb(V)
+	'''
+	U, s, VT = svd(matrix)  #ref https://machinelearningmastery.com/singular-value-decomposition-for-machine-learning/
+	Sigma = diag(s)
+	B = U.dot(Sigma.dot(VT))
+	'''
+
+	
+
+def getvij(h, i, j):
+	vij = [h[0][i] * h[0][j], h[0][i]*h[1][j]  + h[1][i] * h[0][j], h[1][i]*h[1][j], h[2][i]*h[0][j] + h[0][i]*h[2][j], h[2][i]*h[1][j] + h[1][i]*h[2][j], h[2][i]*h[2][j]]
+
+	return vij
+
+def transpose(value):
+	ret = cv2.transpose(np.float32(value))
+	return ret
+
+
+#def littleb(V):
+	#h = cv2.getPerspectiveTransform(chosenWorld, chosenImg)
+
+def getBigB(b):
+
+	#b = b.reshape(6,-1)
+
+	B = [[b[0], b[1], b[3]], [b[1], b[2], b[4]], [b[3], b[4], b[5]]]
+	return B
+
+
+
+def intparameters(B):
+	print("B[0][0] ", B[0])
+
+
+	v0 = (B[1] * B[3] - B[0] * B[4]) / (B[0] * B[2] - B[1]**2)
+	lam = B[5] - (B[3]**2 + v0 * (B[1] * B[3] - B[0] * B[4])) / B[0]
+	print("lam ", lam)
+	alpha = np.sqrt(lam / B[0])
+	beta = np.sqrt((lam * B[0]) / (B[0] * B[2] - B[1]**2))
+	gamma = -B[1] * (alpha ** 2) * beta / lam
+	print("gamma ", lam)
+	u0 = gamma*v0 / beta - B[3] * alpha ** 2/ lam
+
+	#A = [[alpha, gamma, u0], [0, beta, v0], [0, 0, 1]]
+
+	A =[
+		[alpha, gamma, u0],
+		[0, beta, v0],
+		[0, 0, 1]
+		]
+
+	return A, lam
+
+
+
+def getRT(A, h, lam):
+
+	
+	invA = np.linalg.inv(np.float32(np.array(A)))
+	h1 = np.array([h[0][0], h[0][1], h[0][2]])
+	h2 = np.array([h[1][0], h[1][1], h[1][2]])
+	h3 = np.array([h[2][0], h[2][1], h[2][2]])
+
+	#lam = 1 / np.absolute(np.dot(invA ,h1))
+	#lam2 = 1 / np.absolute(np.dot(invA ,h2))  
+
+
+	r1 = np.absolute(np.dot(np.dot(lam, invA), h1))
+	r2 = np.absolute(np.dot(np.dot(lam, invA), h2))
+
+	print("r1 ", r1)
+	print("r2 ", r2)
+
+	r3 = np.cross(r1, r2)
+	t = np.dot(np.dot(lam, invA), h3)
+
+
+	R = [r1, r2, r3, t] #Make 3 by 4 matrix 
+	print("len of R ", R)
+	R = np.array([r1, r2, r3, t])
+	R.reshape(4,-1)
+	print("AFTER RESHAPE ", R)
+
+
+	return R
+
+	
+#def minimize(alpha, beta, u0, v0):
+	#u = u0 + alpha *
 
 
 correspondance()
+
+
+
+
+
+
+
+
+
 
 
 
